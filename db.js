f0a711333bbe02aae2316c0ac89137adb148ccdf
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-02-09 09:53:45 +0530

    added credentials to db

--- db.js
+++ db.js
@@ -1,27 +1,22 @@
+import { Sequelize } from "@sequelize/core";
+import { PostgresDialect } from "@sequelize/postgres";
+import dotenv from "dotenv";
 
-import { Sequelize } from '@sequelize/core';
-import { PostgresDialect } from '@sequelize/postgres';
+dotenv.config();
 
+// const sequelize = new Sequelize({
+//   dialect: PostgresDialect,
+//   database: process.env.DATABASE,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   sync: true,
+// });
 
-
-// export default {
-//     query: (text, params) => pool.query(text, params),
-//     connect:()=>pool.connect()
-//   }
-// import { Sequelize } from '@sequelize/core';
-// import { PostgresDialect } from '@sequelize/postgres';
-// import { Users } from './model/users.js';
-
-
- const sequelize = new Sequelize({
+const sequelize = new Sequelize({
   dialect: PostgresDialect,
-  database: 'blog_app',
-  user: 'test',
-  password: 'test',
-  host: 'localhost',
-  port: 5432,
-  sync:true
+  url: process.env.DATABASE_URL,
 });
-
 
 export default sequelize;

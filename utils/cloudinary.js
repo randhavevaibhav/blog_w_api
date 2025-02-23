d2ae6f9d8d2eb3f8bb1fe0686f3aabe641222c29
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-02-23 13:34:05 +0530

    changed env var names

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,8 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 export async function handleUpload(file) {
   const res = await cloudinary.uploader.upload(file, {

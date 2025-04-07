700b488d4e1d61039b224ae46cf3893e6cccf360
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-04-07 20:12:51 +0530

    removed cloudnary and added supabase for img storage

--- controller/uploadFile/post/uploadFileController.js
+++ controller/uploadFile/post/uploadFileController.js
@@ -1,18 +1,25 @@
 import { handleUpload } from "../../../utils/cloudinary.js";
-
+import { createClient } from "@supabase/supabase-js";
 import { Jimp } from "jimp";
-export const uploadFileController = async (req, res) => {
+import * as path from "path";
+
+import { catchAsync } from "../../../utils/catchAsync.js";
+export const uploadFileController = catchAsync(async (req, res) => {
   if (!req.file) {
     return res.status(200).send({
       message: "No post image added !",
       fileURL: "",
     });
   }
-
+  const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+  );
   const fileBuffer = req.file.buffer;
   const mimetype = req.file.mimetype;
-  const fileName = req.file.filename;
-
+  const fileExt = path.extname(req.file.originalname);
+  const fileName = `${Date.now()}_post_title_img_${fileExt}`;
+  const filePath = `${fileName}`;
   const image = await Jimp.read(fileBuffer);
 
   if (image.width > 900 && image.height > 600) {
@@ -25,13 +32,26 @@ export const uploadFileController = async (req, res) => {
   // Compress and set quality
 
   const compressedImageBuffer = await image.getBuffer(mimetype);
-  const b64 = Buffer.from(compressedImageBuffer).toString("base64");
-  let dataURI = "data:" + mimetype + ";base64," + b64;
-  const cldRes = await handleUpload(dataURI);
+
+  const { data, error } = await supabase.storage
+    .from(process.env.SUPABASE_BUCKET)
+    .upload(filePath, compressedImageBuffer, {
+      contentType: mimetype,
+    });
+
+  if (error) {
+    throw new Error(`Error while uploading file to supabase! ==> ${error}`);
+  }
+
+  const { data: publicUrlData } = supabase.storage
+    .from(process.env.SUPABASE_BUCKET)
+    .getPublicUrl(filePath);
+  const imageUrl = publicUrlData.publicUrl;
 
   res.send({
     message: "File uploaded successfully!",
     file: fileName,
-    fileURL: cldRes.url,
+
+    fileURL: imageUrl,
   });
-};
+});

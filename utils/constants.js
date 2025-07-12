f60ac3ae56d415751c6ab943e92991c7fe89aea7
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-07-12 22:51:11 +0530

    cleanup

--- utils/constants.js
+++ utils/constants.js
@@ -1,13 +1,12 @@
 export const JWT_MAX_AGE = 5 * 60; //max JWT Auth Token age = 5min
-export const JWT_SECRET_KEY = "VTR@3000";
 export const LOCAL_CLIENT_ORIGIN = "http://localhost:5173";
 export const REMOTE_CLIENT_ORIGIN = "https://blog-w-three.vercel.app";
-export const POST_OFFSET=5
-export const FOLLOWERS_OFFSET=5
-export const FOLLOWING_OFFSET=5
-export const COMMENT_OFFSET = 5
-export const COMMENT_LIMIT = 5
-export const POST_LIMIT=5
-export const SEARCH_POST_LIMIT=10
-export const FOLLOWERS_LIMIT=10
-export const FOLLOWING_LIMIT = 10
+export const POST_OFFSET = 5;
+export const FOLLOWERS_OFFSET = 5;
+export const FOLLOWING_OFFSET = 5;
+export const COMMENT_OFFSET = 5;
+export const COMMENT_LIMIT = 5;
+export const POST_LIMIT = 5;
+export const SEARCH_POST_LIMIT = 10;
+export const FOLLOWERS_LIMIT = 10;
+export const FOLLOWING_LIMIT = 10;

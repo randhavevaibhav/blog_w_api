349ec780b009b13cae4d15711cf1c6deb3e4cd21
randhavevaibhav <randhavevaibhav3@gmail.com>
2026-01-31 09:21:32 +0530

    added test client

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,8 @@
 export const JWT_MAX_AGE = 5 * 60; //max JWT Auth Token age = 5min
 export const LOCAL_CLIENT_ORIGIN = "http://localhost:5173";
 export const REMOTE_CLIENT_ORIGIN = "https://blog-w-three.vercel.app";
+export const REMOTE_TEST_CLIENT_ORIGIN =
+  "https://blog-w-git-dev-vaibhavs-projects-ad023c36.vercel.app";
 export const POST_OFFSET = 5;
 export const COMMENT_OFFSET = 10;
 export const COMMENT_LIMIT = 10;

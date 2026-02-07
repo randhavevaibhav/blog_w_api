0cbd7fd292b5e34a24599c3cf852b0f19bf1a37a
randhavevaibhav <randhavevaibhav3@gmail.com>
2026-02-07 21:39:08 +0530

    removed pre-render

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ import {
 } from "./utils/constants.js";
 import { globalErrorController } from "./controller/error/globalErrorController.js";
 import rateLimit from "express-rate-limit";
-import prerender from "prerender-node";
+
 const limiter = rateLimit({
   windowMs: 1000, // 1 sec
   limit: 20, // each IP can make up to 20 requests per `windowsMs` (1 sec)
@@ -54,7 +54,6 @@ const corsOptions = {
 };
 
 const PORT = 8003;
-prerender.set("prerenderToken", process.env.PRERENDER_TOKEN);
 
 app.use(cors(corsOptions));
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -71,7 +70,6 @@ app.use(commentLikesRoute);
 app.use(refreshTokenRoute);
 app.use(uploadFileRoute);
 app.use(convertRoutes);
-app.use(prerender);
 
 app.get("/", (req, res) => {
   res.send({

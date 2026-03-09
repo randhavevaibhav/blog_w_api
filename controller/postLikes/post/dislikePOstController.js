2952e95bfc667715d1588d4ef667f7120ed96a32
randhavevaibhav <randhavevaibhav3@gmail.com>
2026-03-09 23:12:08 +0530

    added constrains for like/dislike post

--- controller/postLikes/post/dislikePOstController.js
+++ controller/postLikes/post/dislikePOstController.js
@@ -1,26 +1,20 @@
 import {
-  checkIfPostLikedByUser,
   removeUserPostLike,
 } from "../../../model/PostLikes/quires.js";
 import { catchAsync } from "../../../utils/catchAsync.js";
 
-
 export const dislikePostController = catchAsync(async (req, res) => {
   const {userId} = req.user;
   const { postId } = req.body;
-
-  const isPostLiked = await checkIfPostLikedByUser({ userId, postId });
-
-  if (!isPostLiked) {
-    //already dis-liked !
-    return res.sendStatus(204);
-  }
-
+ 
   const removePostLikeResult = await removeUserPostLike({ userId, postId });
 
-  // console.log("result in removePostLikeResult =======> ",removePostLikeResult);
-  // console.log("result in removePostLikeResult =======> ",decPostLikeResult);
-
+  if(removePostLikeResult===0)
+  {
+    return res.status(200).send({
+      message:"not modified"
+    })
+  }
   return res.status(200).send({
     message: "disliked a post !",
     liked: false,
--- controller/postLikes/post/likePostController.js
+++ controller/postLikes/post/likePostController.js
@@ -1,27 +1,23 @@
 import {
   createPostLike,
-  checkIfPostLikedByUser,
 } from "../../../model/PostLikes/quires.js";
-
 import { catchAsync } from "../../../utils/catchAsync.js";
 
 export const likePostController = catchAsync(async (req, res) => {
   const { userId } = req.user;
   const { postId } = req.body;
-
-  //check if post is already liked by user
-  const isPostLiked = await checkIfPostLikedByUser({ userId, postId });
-
-  if (isPostLiked) {
-    //already liked !
-    return res.sendStatus(204);
-  }
-
-  //if not like post
+ 
   const createPostLikeResult = await createPostLike({
     userId,
     postId,
   });
+
+   if(createPostLikeResult===0)
+  {
+    return res.status(200).send({
+      message:"not modified"
+    })
+  }
 
   return res.status(200).send({
     message: "liked a post !",
